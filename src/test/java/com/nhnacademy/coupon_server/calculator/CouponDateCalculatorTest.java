d471a2c3471a7d62a3c0e03b99a48fc065665623
Poly-Etilen <wlsdud04060@naver.com>
2025-12-15 11:20:49 +0900

    fix : 테스트 코드 수정

--- src/test/java/com/nhnacademy/coupon_server/calculator/CouponDateCalculatorTest.java
+++ src/test/java/com/nhnacademy/coupon_server/calculator/CouponDateCalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class CouponDateCalculatorTest {
     private final CouponDateCalculator couponDateCalculator = new CouponDateCalculator();
@@ -34,10 +35,12 @@ public class CouponDateCalculatorTest {
                 .build();
 
         LocalDateTime result = couponDateCalculator.calculateExpiration(coupon);
-        LocalDateTime expected = LocalDateTime.now().plusDays(periodDays);
 
-        Assertions.assertTrue(result.isAfter(expected.minusSeconds(1)));
-        Assertions.assertTrue(result.isBefore(expected.plusSeconds(1)));
+        LocalDateTime expectedDate = LocalDateTime.now().plusDays(periodDays);
+        LocalDateTime expected = expectedDate.with(LocalTime.MAX);
+
+        Assertions.assertEquals(expected.toLocalDate(), result.toLocalDate());
+        Assertions.assertEquals(expected.toLocalTime(), result.toLocalTime());
     }
 
     @Test
