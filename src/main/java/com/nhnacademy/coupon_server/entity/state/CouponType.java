93d3eeba9d774c7bfca8c5e5e678298ba7bae265
Poly-Etilen <wlsdud04060@naver.com>
2026-01-03 14:41:01 +0900

    fix: 프론트서버 통신 부분 수정중

--- src/main/java/com/nhnacademy/coupon_server/entity/state/CouponType.java
+++ src/main/java/com/nhnacademy/coupon_server/entity/state/CouponType.java
@@ -5,7 +5,8 @@ public enum CouponType {
     WELCOME("웰컴 쿠폰"), // 기존 Comment.WELCOME 대체
     BIRTHDAY("생일 쿠폰"), // 기존 Comment.BIRTHDAY 대체
     BOOK_SPECIFIC("도서 전용 쿠폰"),
-    CATEGORY_SPECIFIC("카테고리 전용 쿠폰");
+    CATEGORY_SPECIFIC("카테고리 전용 쿠폰"),
+    PRIVATE("비공개 쿠폰");
 
     private final String description;
 
